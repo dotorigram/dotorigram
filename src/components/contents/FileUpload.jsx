b7f6b1182cf39a9413a6540d04245639f71d1694
zi-zzang <kjhwlgusdl@gmail.com>
2022-10-14 10:08:13 +0900

    elements, 정규식 삭제

---
+++ src/components/contents/FileUpload.jsx
@@ -0,0 +1,11 @@
+import React, { useRef } from 'react';
+
+const FileUpload = () => {
+  return (
+    <div>
+      <input type='file' accept='image/*' className='' />
+    </div>
+  );
+};
+
+export default FileUpload;
--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import Mypage from '../pages/Mypage';
 
-const PrivateRoute = ({authenticate}) => {
-  return (
-    authenticate == true? <Mypage /> : <Navigate to="/login" />
-  )
-}
+const PrivateRoute = ({ authenticate }) => {
+  return authenticate === true ? <Mypage /> : <Navigate to='/login' />;
+};
 
 export default PrivateRoute;
