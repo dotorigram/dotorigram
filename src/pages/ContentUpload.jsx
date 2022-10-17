a6902b317e7fa3e223824c9ee628fedd1899fba1
zi-zzang <kjhwlgusdl@gmail.com>
2022-10-17 14:51:25 +0900

    이미지 조건문 추가

--- src/pages/ContentUpload.jsx
+++ src/pages/ContentUpload.jsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useRef, useState } from "react";
-import CloseIcon from "@mui/icons-material/Close";
-import { useDispatch } from "react-redux";
+import React, { useCallback, useRef, useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import { useDispatch } from 'react-redux';
 
 function ContentUpload() {
   // 미리보기
-  const [imageSrc, setImageSrc] = useState("");
+  const [imageSrc, setImageSrc] = useState('');
   const onImgInputBtnClick = (e) => {
     e.preventDefault();
     logoImgInput.current.click();
@@ -22,41 +22,38 @@ function ContentUpload() {
   };
 
   return (
-    <div className="mx-auto max-w-470 relative">
-      <div className="flex justify-center mt-2 border-b static">
-        <div className="mb-2 font-bold static">새 게시물 만들기</div>
-      </div>
-      <div className="flex justify-center h-96 flex-col items-center gap-y-2 static">
-        <img
-          className="w-20 h-18"
-          src={require("../static/img/addPost.PNG")}
-          alt="img"
-        />
-        <h1 margin="20px" F_size="22px">
-          버튼을 눌러 사진을 추가하세요
-        </h1>
-
-        <div className="static w-80 h-80">
-          {imageSrc && <img src={imageSrc} alt="" />}
+    <div className='mx-auto max-w-470'>
+      <div className='flex justify-center mt-2 border-b'>
+        <div className='mb-2 font-bold'>
+          새 게시물 만들기
+          {/* <span className='absolute'>공유하기</span> */}
+          {/* 공유하기 버튼이 필요한데 말이죠.. */}
         </div>
-        <input
-          type="file"
-          accept="image/*"
-          className="hidden"
-          onChange={(e) => encodeFileToBase64(e.target.files[0])}
-          ref={logoImgInput}
-        />
-        <button
-          className="font-semibold text-white bg-blue-500 rounded w-32 h-8"
-          onClick={onImgInputBtnClick}
-        >
-          컴퓨터에서 선택
-        </button>
       </div>
-      <textarea
-        className="outline-none w-full h-96 text-xl placeholder:text-base"
-        placeholder="문구 입력..."
-      ></textarea>
+      <div className='flex justify-center h-96 flex-col items-center gap-y-2'>
+        <div className=' w-80 h-80'>
+          {imageSrc ? (
+            <div>
+              <img className='h-45' src={imageSrc} alt='' />
+              <button className='font-semibold text-white bg-blue-500 rounded w-32 h-8' onClick={onImgInputBtnClick}>
+                컴퓨터에서 선택
+              </button>
+            </div>
+          ) : (
+            <div>
+              <img className='w-20 h-18' src={require('../static/img/addPost.PNG')} alt='img' />
+              <h1 margin='20px' F_size='22px'>
+                버튼을 눌러 사진을 추가하세요
+              </h1>
+              <button className='font-semibold text-white bg-blue-500 rounded w-32 h-8' onClick={onImgInputBtnClick}>
+                컴퓨터에서 선택
+              </button>
+            </div>
+          )}
+        </div>
+        <input type='file' accept='image/*' className='hidden' onChange={(e) => encodeFileToBase64(e.target.files[0])} ref={logoImgInput} />
+      </div>
+      <textarea className='outline-none w-full h-96 text-xl placeholder:text-base' placeholder='문구 입력...'></textarea>
     </div>
   );
 }
