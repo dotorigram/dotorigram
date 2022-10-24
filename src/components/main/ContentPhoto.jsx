c85c4de0faec71083c7fc30acf4bdc1ca512ed80
yurimpark <yuriming112@gmail.com>
2022-10-24 19:29:21 +0900

    머지 에러 수정

--- src/components/main/ContentPhoto.jsx
+++ src/components/main/ContentPhoto.jsx
@@ -4,12 +4,6 @@ import { useDispatch } from "react-redux";
 import { getPost } from "../../redux/reducer/modules/postReducer";
 import { Grid } from "@mui/material";
 
-import React, { useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { getPost } from '../../redux/reducer/modules/postReducer';
-import { Grid } from '@mui/material';
-
 // import { useSelector } from 'react-redux';
 const ContentPhoto = ({ post }) => {
   // const { post } = useSelector((state) => state.post);
@@ -17,17 +11,9 @@ const ContentPhoto = ({ post }) => {
   const dispatch = useDispatch();
   const location = useLocation();
 
-  return (
-    <div className='flex items-center'>
-      <img src={post?.img} className='m-auto' alt='' onClick={() => navigate('/postedcontent')} />
-      {/* <img src={require('../../static/img/feed_img.jpg')} className='w-100 h-100 m-auto' alt='' onClick={() => navigate('/postedcontent')} /> */}
-    </div>
-  );
-
   useEffect(() => {
     dispatch(getPost());
   }, [dispatch]);
-
 
   if (location.pathname === "/") {
     // console.log(post);
@@ -46,13 +32,11 @@ const ContentPhoto = ({ post }) => {
     console.log(post);
     return (
       <div className="grid grid-cols-3 gap-5">
-
         {post.map((item, index) => {
           //   console.log(item);
           return (
             <div key={index}>
               <div
-
                 style={{
                   backgroundImage: `url(${item.img})`,
                   backgroundSize: "cover",
@@ -61,7 +45,6 @@ const ContentPhoto = ({ post }) => {
                 className="w-40 h-40"
                 key={index}
                 onClick={() => navigate("/postedcontent")}
-
               ></div>
             </div>
           );
